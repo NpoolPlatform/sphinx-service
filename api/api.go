2dee8666c1a69fb9501e4302f0e75ee704ff76e6
Cyvadra <dj@lifesdk.com>
2021-11-02 18:21:07 +0800

    rename package

--- api/api.go
+++ api/api.go
@@ -2,17 +2,17 @@ package api
 
 import (
 	"context"
-
-	"github.com/NpoolPlatform/go-service-app-template/api/sphinx"
-	"github.com/NpoolPlatform/go-service-app-template/message/npool"
+	"github.com/NpoolPlatform/sphinx-service/message/npool"
+	"github.com/NpoolPlatform/sphinx-service/pkg/core"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 )
 
 func Register(server grpc.ServiceRegistrar) {
-	npool.RegisterServiceExampleServer(server, &sphinx.Server{})
+	npool.RegisterTradingServer(server, core.GetTradingServer())
 }
 
 func RegisterGateway(mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) error {
-	return npool.RegisterServiceExampleHandlerFromEndpoint(context.Background(), mux, endpoint, opts)
+	return npool.RegisterTradingHandlerFromEndpoint(context.Background(), mux, endpoint, opts)
+	// return npool.RegisterServiceExampleHandlerFromEndpoint(context.Background(), mux, endpoint, opts)
 }
