9a1c97bb0033706365cafb6f73d4b7669f328545
Cyvadra <dj@lifesdk.com>
2021-11-03 21:23:40 +0800

    bugfix

--- api/querys.go
+++ api/querys.go
@@ -1,11 +1,11 @@
-package echo
+package api
 
 import (
 	"context"
 
 	"github.com/NpoolPlatform/sphinx-service/message/npool"
 
-	"github.com/NpoolPlatform/cloud-hashing-goods/pkg/core"
+	"github.com/NpoolPlatform/sphinx-service/pkg/core"
 )
 
 // https://github.com/grpc/grpc-go/issues/3794
@@ -16,8 +16,10 @@ type Server struct {
 
 func (s *Server) GetCoinInfos(ctx context.Context, req *npool.GetCoinInfosRequest) (cilist *npool.CoinInfoList, err error) {
 	cilist, err = core.GetCoinInfos(ctx, req)
+	return
 }
 
 func (s *Server) GetCoinInfo(ctx context.Context, req *npool.GetCoinInfoRequest) (cilist *npool.CoinInfoRow, err error) {
 	cilist, err = core.GetCoinInfo(ctx, req)
+	return
 }
