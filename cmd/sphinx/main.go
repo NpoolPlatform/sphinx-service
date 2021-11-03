c5d9a0efafd0ede3df7ee3da11cf894e30e7dcdc
Cyvadra <dj@lifesdk.com>
2021-11-03 21:10:53 +0800

    Modify 'serviceName'

--- cmd/sphinx/main.go
+++ cmd/sphinx/main.go
@@ -48,30 +48,51 @@ package main
 import (
 	"fmt"
 	"os"
+	"path"
+	"runtime"
+
 	"github.com/NpoolPlatform/go-service-framework/pkg/app"
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 	cli "github.com/urfave/cli/v2"
 )
 
 const (
-	serviceName = "Sphinx"
+	serviceName = "sphinx"
 	description = "Trading service and Wallet agent."
+	usageText   = ""
 )
 
 func main() {
+	var err error
+	// get commands
 	commands := cli.Commands{
 		runCmd,
 	}
-
-	description := fmt.Sprintf("my %v service cli\nFor help on any individual command run <%v COMMAND -h>\n",
-		serviceName, serviceName)
-	err := app.Init(serviceName, description, "", "", nil, commands)
+	// get path
+	_, mainPath, _, ok := runtime.Caller(0)
+	if !ok {
+		logger.Sugar().Errorf("cannot get path of main.go")
+		return
+	}
+	err = app.Init(serviceName, description, "", "", path.Dir(mainPath), nil, commands)
 	if err != nil {
 		logger.Sugar().Errorf("fail to create %v: %v", serviceName, err)
 		return
 	}
+	// genereate description
+	argsUsage := fmt.Sprintf("my %v service cli\nFor help on any individual command run <%v COMMAND -h>\n",
+		serviceName, serviceName)
+	// create service
+	err = app.Init(serviceName, description, usageText, argsUsage, path.Dir(mainPath), nil, commands)
+	if err != nil {
+		logger.Sugar().Errorf("fail to create %v: %v", serviceName, err)
+		return
+	}
+	// run service
 	err = app.Run(os.Args)
 	if err != nil {
 		logger.Sugar().Errorf("fail to run %v: %v", serviceName, err)
+	} else {
+		logger.Sugar().Infof("run main.go from: %v", mainPath)
 	}
 }
