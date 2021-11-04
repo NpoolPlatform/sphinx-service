292eb33c4ca307126d2a02cf5184216153e37139
Cyvadra <dj@lifesdk.com>
2021-11-04 11:52:27 +0800

    arrange modules; currently stuck on deps, shall procedure sphinx-plugin(agent) first

--- pkg/core/01_init.go
+++ pkg/core/01_init.go
@@ -1,62 +1,14 @@
 package core
 
 import (
-	"github.com/NpoolPlatform/sphinx-service/message/npool"
 	"github.com/NpoolPlatform/sphinx-service/pkg/db"
+	"github.com/NpoolPlatform/sphinx-service/pkg/db/ent"
 )
 
-type Server struct {
-	npool.UnimplementedTradingServer
-}
 
-var PendingServer *UnimplementedTradingServer
+
+var Client *ent.Client
 
 func init() {
-	PendingServer := &UnimplementedTradingServer{}
-
-}
-
-func GetTradingServer() *TradingServer {
-	return Server
-}
-
-
-
-
-
-// 没写完的放这里，写完删
-
-// 创建账户
-func (s *Server) RegisterAccount(context.Context, *RegisterAccountRequest) (*AccountAddress, error) {
-	return nil, nil
-}
-
-// 余额查询
-func (s *Server) GetBalance(context.Context, *GetBalanceRequest) (*AccountBalance, error) {
-	return nil, nil
-}
-
-// 转账 / 提现
-func (s *Server) ApplyTransaction(context.Context, *ApplyTransactionRequest) (*emptypb.Empty, error) {
-	return nil, nil
-}
-
-// 签名服务接入点
-func (s *Server) PortalSign(context.Context, *PortalSignInit) (*IdentityProof, error) {
-	return nil, nil
-}
-
-// 代理服务接入点
-func (s *Server) PortalWallet(context.Context, *PortalWalletInit) (*IdentityProof, error) {
-	return nil, nil
-}
-
-// 账户交易查询
-func (s *Server) GetTxJSON(context.Context, *GetTxJSONRequest) (*AccountTxJSON, error) {
-	return nil, nil
-}
-
-// 交易状态查询
-func (s *Server) GetInsiteTxStatus(context.Context, *GetInsiteTxStatusRequest) (*GetInsiteTxStatusResponse, error) {
-	return nil, nil
+	Client = db.Client()
 }
--- pkg/core/querys.go
+++ pkg/core/querys.go
@@ -4,21 +4,14 @@ import (
 	"context"
 
 	"github.com/NpoolPlatform/sphinx-service/message/npool"
-	"github.com/NpoolPlatform/sphinx-service/pkg/db"
 	"github.com/NpoolPlatform/sphinx-service/pkg/db/ent"
 	"github.com/NpoolPlatform/sphinx-service/pkg/db/ent/coininfo"
 )
 
-var client *ent.Client
-
-func init() {
-	client = db.Client()
-}
-
 // 查询全部币种
 func GetCoinInfos(ctx context.Context, req *npool.GetCoinInfosRequest) (cilist *npool.CoinInfoList, err error) {
 	ent_resp := []*ent.CoinInfo{}
-	ent_resp, err = client.CoinInfo.Query().All(ctx)
+	ent_resp, err = Client.CoinInfo.Query().All(ctx)
 	var tmp_cir []*npool.CoinInfoRow
 	for _, row := range ent_resp {
 		tmp_cir = append(tmp_cir, &npool.CoinInfoRow{
@@ -37,7 +30,7 @@ func GetCoinInfos(ctx context.Context, req *npool.GetCoinInfosRequest) (cilist *
 // 查询单个币种
 func GetCoinInfo(ctx context.Context, req *npool.GetCoinInfoRequest) ( coin_info *npool.CoinInfoRow, err error) {
 	ent_resp := &ent.CoinInfo{}
-	ent_resp, err = client.CoinInfo.
+	ent_resp, err = Client.CoinInfo.
 	Query().
 	Where(
 			coininfo.ID(req.CoinId),
@@ -50,4 +43,3 @@ func GetCoinInfo(ctx context.Context, req *npool.GetCoinInfoRequest) ( coin_info
 	}
 	return coin_info, err
 }
-
