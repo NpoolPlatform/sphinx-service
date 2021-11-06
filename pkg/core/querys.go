4ad5354f63af239f5d945bfcdd99b6e8b8121dc0
Cyvadra <dj@lifesdk.com>
2021-11-06 14:36:44 +0800

    test push

--- pkg/core/querys.go
+++ pkg/core/querys.go
@@ -10,9 +10,9 @@ import (
 // 查询全部币种
 func GetCoinInfos(ctx context.Context, req *npool.GetCoinInfosRequest) (cilist *npool.CoinInfoList, err error) {
 	entResp, err := Client.CoinInfo.Query().All(ctx)
-	tmp_cir := make([]*npool.CoinInfoRow, len(entResp))
+	tmpCIR := make([]*npool.CoinInfoRow, len(entResp))
 	for i, row := range entResp {
-		tmp_cir[i] = &npool.CoinInfoRow{
+		tmpCIR[i] = &npool.CoinInfoRow{
 			Id:           row.ID,
 			Name:         row.Name,
 			Unit:         row.Unit,
@@ -20,19 +20,19 @@ func GetCoinInfos(ctx context.Context, req *npool.GetCoinInfosRequest) (cilist *
 		}
 	}
 	cilist = &npool.CoinInfoList{
-		List: tmp_cir,
+		List: tmpCIR,
 	}
 	return
 }
 
 // 查询单个币种
-func GetCoinInfo(ctx context.Context, req *npool.GetCoinInfoRequest) (coin_info *npool.CoinInfoRow, err error) {
+func GetCoinInfo(ctx context.Context, req *npool.GetCoinInfoRequest) (coinInfo *npool.CoinInfoRow, err error) {
 	entResp, err := Client.CoinInfo.
 		Query().
 		Where(
 			coininfo.ID(req.CoinId),
 		).First(ctx)
-	coin_info = &npool.CoinInfoRow{
+	coinInfo = &npool.CoinInfoRow{
 		Id:           entResp.ID,
 		NeedSigninfo: entResp.NeedSigninfo,
 		Name:         entResp.Name,
