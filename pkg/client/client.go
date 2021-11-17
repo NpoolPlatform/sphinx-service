3981b1642b553ea75cef80c773e034a81b46d6f5
Cyvadra <dj@lifesdk.com>
2021-11-17 16:24:03 +0800

    Seperate grpc clients

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -3,37 +3,37 @@ package client
 import (
 	"github.com/NpoolPlatform/go-service-framework/pkg/grpc"
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	"github.com/NpoolPlatform/message/npool/coininfo"
+	"github.com/NpoolPlatform/message/npool/signproxy"
 	grpcBase "google.golang.org/grpc"
 )
 
-var ClientApproval *grpcBase.ClientConn
+var (
+	ClientApproval *grpcBase.ClientConn
+	ClientProxy    signproxy.SignProxyClient
+	ClientCoinInfo coininfo.SphinxCoininfoClient
+)
 
 // 审核服务 grpc远程调用
 
-func GetApprovalClient() *grpcBase.ClientConn {
-	ClientApproval.Connect()
-	return ClientApproval
-}
-
 func InitApprovalClient() (err error) {
-	// get service conn
 	_, err = grpc.GetGRPCConn("TO-BE-IMPLEMENTED")
 	if err != nil {
 		logger.Sugar().Errorf("get grpc connection failure, err: %v", err)
 		return
 	}
-	// get service client
-	// ClientApproval = signproxy.NewSphinxSignproxyClient(conn)
+	// MARK
 	return
 }
 
-// MARK
-func LetApproveTransaction(transactionIDInsite string) (err error) {
-	return nil
-}
-
-// 钱包代理服务
+// 钱包代理服务 grpc远程调用 其余在rabbitmq做消息通知
 
 func InitProxyClient() (err error) {
-	return nil
+	conn, err := grpc.GetGRPCConn("TO-BE-IMPLEMENTED")
+	if err != nil {
+		logger.Sugar().Errorf("get grpc connection failure, err: %v", err)
+		return
+	}
+	ClientProxy = signproxy.NewSignProxyClient(conn)
+	return
 }
