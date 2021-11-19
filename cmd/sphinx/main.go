7f94d2a14dfff2e71ebd1fc6e6538f49c951167e
Cyvadra <dj@lifesdk.com>
2021-11-19 19:39:44 +0800

    bugfix

--- cmd/sphinx/main.go
+++ cmd/sphinx/main.go
@@ -5,13 +5,16 @@ import (
 	"os"
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/app"
+	"github.com/NpoolPlatform/go-service-framework/pkg/config"
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	mysqlconst "github.com/NpoolPlatform/go-service-framework/pkg/mysql/const"
+	rabbitmqconst "github.com/NpoolPlatform/go-service-framework/pkg/rabbitmq/const"
+	redisconst "github.com/NpoolPlatform/go-service-framework/pkg/redis/const"
 	cli "github.com/urfave/cli/v2"
 )
 
 const (
-	serviceName = "Sphinx"
-	usageText   = "Trading service and Wallet agent."
+	serviceName = "Sphinx Service"
 )
 
 func main() {
@@ -21,7 +24,10 @@ func main() {
 
 	description := fmt.Sprintf("my %v service cli\nFor help on any individual command run <%v COMMAND -h>\n",
 		serviceName, serviceName)
-	err := app.Init(serviceName, description, usageText, "", "./", nil, commands)
+	err := app.Init(serviceName, description, "", "", "./", nil, commands,
+		config.ServiceNameToNamespace(mysqlconst.MysqlServiceName),
+		config.ServiceNameToNamespace(redisconst.RedisServiceName),
+		config.ServiceNameToNamespace(rabbitmqconst.RabbitMQServiceName))
 	if err != nil {
 		logger.Sugar().Errorf("fail to create %v: %v", serviceName, err)
 		return
