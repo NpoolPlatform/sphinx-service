06e491f9253d35ec2f4471a368bfd984bf7f2b28
linuxpizi <1360447275@qq.com>
2021-12-07 22:53:00 +0800

    update dep

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.16
 
 require (
 	entgo.io/ent v0.9.1
-	github.com/NpoolPlatform/go-service-framework v0.0.0-20211204042807-786e5a616191
+	github.com/NpoolPlatform/go-service-framework v0.0.0-20211207121121-adb2402676f0
 	github.com/NpoolPlatform/message v0.0.0-20211206113744-0a03f0f0d536
 	github.com/NpoolPlatform/sphinx-coininfo v0.0.0-20211205081309-f78a3a8955cf
 	github.com/NpoolPlatform/sphinx-proxy v0.0.0-20211205102315-ca5ce09a5bb2
--- pkg/tasks/sync_transaction.go
+++ pkg/tasks/sync_transaction.go
@@ -1,9 +1,11 @@
 package tasks
 
 import (
+	"context"
 	"time"
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	"github.com/NpoolPlatform/sphinx-service/pkg/crud"
 	constant "github.com/NpoolPlatform/sphinx-service/pkg/message/const"
 )
 
@@ -15,7 +17,28 @@ func init() {
 func syncTransaction() {
 	for range time.NewTicker(constant.TaskDuration).C {
 		func() {
-			logger.Sugar().Infof("syncTransaction")
+			ctx, cancel := context.WithTimeout(context.Background(), constant.GrpcTimeout)
+			defer cancel()
+			transInfos, _, err := crud.GetTransactions(ctx, &crud.GetTransactionsParams{})
+			if err != nil {
+				logger.Sugar().Infof("err:%v", err)
+			}
+
+			for _, trans := range transInfos {
+				logger.Sugar().Infof("info: %v", trans)
+				// conn, err := grpc.GetGRPCConn(pconst.ServiceName, grpc.GRPCTAG)
+				// if err != nil {
+
+				// }
+
+				// client := sphinxproxy.NewSphinxProxyClient(conn)
+				// tranInfo, err := client.GetTransaction(ctx, &sphinxproxy.GetTransactionRequest{
+				// 	TransactionID: trans.TransactionID,
+				// })
+				// if err != nil {
+
+				// }
+			}
 		}()
 	}
 }
